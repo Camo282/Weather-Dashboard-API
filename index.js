2d7aaac835cb6f350e84638010c2b8cf127ecaac
Camo282 <camo282@gmail.com>
2022-10-17 14:49:58 -0400

    search button submission

--- index.js
+++ index.js
@@ -159,3 +159,21 @@ var fiveDayForecastSection = function(cityName) {
                 })
         })
 };
+
+// called when the search form is submitted
+$("#search-form").on("submit", function() {
+    event.preventDefault();
+    
+    // get name of city searched
+    var cityName = $("#search-input").val();
+
+    if (cityName === "" || cityName == null) {
+        //send alert if search input is empty when submitted
+        alert("Please enter name of city.");
+        event.preventDefault();
+    } else {
+        // if cityName is valid, add it to search history list and display its weather conditions
+        currentWeatherSection(cityName);
+        fiveDayForecastSection(cityName);
+    }
+});
